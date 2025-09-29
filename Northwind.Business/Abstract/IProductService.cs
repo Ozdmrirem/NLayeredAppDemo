7d888644abd39732530a1158b7dfbaa4f1cd4377
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-09-30 01:35:12 +0300

    Add files via upload

--- Northwind.Business/Abstract/IProductService.cs
+++ Northwind.Business/Abstract/IProductService.cs
@@ -9,7 +9,12 @@ namespace Northwind.Business.Abstract
 {
     public interface IProductService
     {
+        
         List<Product> GetAll();
         List<Product> GetProductsByCategory(int categoryId);
+        List<Product> GetProductsByProductName(string productName);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
     }
 }
--- Northwind.Business/Concrete/ProductManager.cs
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -5,19 +5,39 @@ using Northwind.DataAccess.Concrete.EntityFramework;
 using Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Northwind.Business.Concrete
 {
-    public class ProductManager:IProductService
+    public class ProductManager : IProductService
     {
         private IProductDal _productDal;
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
         }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                _productDal.Delete(product);
+            }
+            catch (DbUpdateException e)
+            {
+                throw new Exception("Silme işlemi gerçekleştirilemedi", e);
+            }
+            
+        }
+
         public List<Product> GetAll()
         {
             //Business code
@@ -27,6 +47,16 @@ namespace Northwind.Business.Concrete
         public List<Product> GetProductsByCategory(int categoryId)
         {
             return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetProductsByProductName(string productName)
+        {
+            return _productDal.GetAll(p => p.ProductName.ToLower().Contains(productName.ToLower()));
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
         }
     }
 }
